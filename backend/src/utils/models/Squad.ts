9b6114108a78de6c6c7c11e9f251f5d3f9d79033
Clyde McNeil <cmcneil2@cnm.edu>
2022-08-28 11:45:59 -0500

    updated squad.ts file

--- backend/src/utils/models/Squad.ts
+++ backend/src/utils/models/Squad.ts
@@ -6,7 +6,7 @@ export interface Squad {
   squadAchievements: string,
   squadEmblem: string | null,
   squadMaxSize: string,
-  squadName: string | null,
+  squadName: string | null
 
 }
 
@@ -34,27 +34,27 @@ export async function insertSquad (squad: Squad): Promise<string> {
   const {squadId, squadProfileId, squadAchievements, squadEmblem, squadMaxSize, squadName  } = squad
   await sql`
   INSERT INTO profile("squadId", "squadProfileId", "squadAchievements", "squadEmblem", "squadMaxSize", "squadName")
-  VALUES(gen_random_uuid(), ${squadProfileId}, ${squadAchievements}, ${squadEmblem}, ${squadMaxSize}, ${squadName})`
+  VALUES(gen_random_uuid(), (gen_randum_uuid(), ${squadAchievements}, ${squadEmblem}, ${squadMaxSize}, ${squadName})`
   return 'Squad successfully created'
 }
-/**
- * Helper function that interacts with postgres to update a profile object in the database
- * @param squad Squad object that will be updated into the database
- * @return success Message.ts if the sql statement was executed with no errors
- **/
-export async function updateSquad (squad: Squad): Promise<string> {
-  const {squadId, squadProfileId, squadAchievements, squadEmblem, squadMaxSize, squadName } = squad
-  await sql`
-UPDATE "squad" 
-SET "squadId" = ${squadId}, "squadProfileId" = ${squadProfileId}, "squadAchievements" = ${squadAchievements}, "squadEmblem" = ${squadEmblem}, "squadMaxSize" = ${squadMaxSize}, "squadName" = ${squadName}
-WHERE "squadId" = ${squadId}`
-  return 'Squad updated successfully'
-}
-/**
- * Helper function that interacts with postgres to select a profile object by its primary key.
- * @param squadId a string containing the primary key for the target object.
- * @return A promise containing a status object with the primary key provided or null if no id was found
- **/
+// /**
+//  * Helper function that interacts with postgres to update a profile object in the database
+//  * @param squad Squad object that will be updated into the database
+//  * @return success Message.ts if the sql statement was executed with no errors
+//  **/
+// export async function updateSquad (squad: Squad): Promise<string> {
+//   const {squadId, squadProfileId, squadAchievements, squadEmblem, squadMaxSize, squadName } = squad
+//   await sql`
+// UPDATE "squad"
+// SET "squadId" = ${squadId}, "squadProfileId" = ${squadProfileId}, "squadAchievements" = ${squadAchievements}, "squadEmblem" = ${squadEmblem}, "squadMaxSize" = ${squadMaxSize}, "squadName" = ${squadName}
+// WHERE "squadId" = ${squadId}`
+//   return 'Squad updated successfully'
+// }
+// /**
+//  * Helper function that interacts with postgres to select a profile object by its primary key.
+//  * @param squadId a string containing the primary key for the target object.
+//  * @return A promise containing a status object with the primary key provided or null if no id was found
+//  **/
 /*
 export async function selectPartialProfileByProfileId (profileId: string): Promise<PartialProfile|null> {
   const result = await sql<Profile[]>`SELECT "profileId", "profileAboutMe", "profileEmail", "profileName" from profile WHERE "profileId" = ${profileId}`
@@ -71,10 +71,3 @@ export async function selectPartialProfileByProfileId (profileId: string): Promi
 //   const result = await sql <Profile[]>`SELECT "profileId", "profileEmail", "profileHash", "profileName" from profile WHERE "profileEmail" = ${profileEmail}`
 //   return result?.length === 1 ? result[0] : null
 // }
-
-
-
-
-
-
-
