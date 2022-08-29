f224c130a4c2206c3abe141932679359a045605c
Clyde McNeil <cmcneil2@cnm.edu>
2022-08-29 07:51:43 -0500

    fixed getSquadBySquadId function, now working back on updating squad

--- backend/src/apis/squad/squad.controller.ts
+++ backend/src/apis/squad/squad.controller.ts
@@ -17,54 +17,56 @@ import { Profile } from '../../utils/models/Profile'
  * @return A promise containing a status object with either a success or failure message set to the message field
  */
 // Export async function for creating a Squad.
-// export async function postSquadController(request: Request, response: Response) : Promise<Response<Status>> {
-//   try {
-//     const {squadName, squadAchievements, squadMaxSize} = request.body;
-//     const profile : Profile = request.session.profile as Profile
-//     const squadProfileId : string = <string>profile.profileId
-//     const squad: Squad = {
-//       squadId: null,
-//       squadProfileId,
-//       squadAchievements,
-//       squadEmblem: null,
-//       squadMaxSize,
-//       squadName,
-//     }
-//     const result = await insertSquad(squad)
-//     const status: Status = {
-//       status: 200,
-//       message: result,
-//       data: null
-//     };
-//     return response.json(status);
-//   } catch(error) {
-//     return  response.json({
-//       status: 500,
-//       message: "Error Creating squad try again later.",
-//       data: null
-//     })
+export async function postSquadController(request: Request, response: Response) : Promise<Response<Status>> {
+  try {
+    const { squadName, squadAchievements, squadMaxSize } = request.body;
+    const profile: Profile = request.session.profile as Profile
+    const squadProfileId: string = <string>profile.profileId
+    const squad: Squad = {
+      squadId: null,
+      squadProfileId,
+      squadAchievements,
+      squadEmblem: null,
+      squadMaxSize,
+      squadName,
+    }
+    const result = await insertSquad(squad)
+    const status: Status = {
+      status: 200,
+      message: result,
+      data: null
+    };
+    return response.json(status);
+  } catch (error) {
+    return response.json({
+      status: 500,
+      message: "Error Creating squad try again later.",
+      data: null
+    })
+  }
+}
 
-    export async function putSquadController (request: Request, response: Response): Promise<Response> {
-      try {
-        const { profileId } = request.params
-        console.log("squadId from url", profileId)
-        const profile = request.session.profile as Profile
-        const profileIdFromSession = profile.profileId as string
+export async function putSquadController (request: Request, response: Response): Promise<Response> {
+    try {
+      const { profileId } = request.params
+      console.log("squadId from url", profileId)
+      const profile = request.session.profile as Profile
+      const profileIdFromSession = profile.profileId as string
 
-        if (profileId !== profileIdFromSession) {
-          return response.json({ status: 400, data: null, message: 'You are not allowed to preform this task' })
-        }
-    // Export async function for updating a Squad
-        const {squadAchievements, squadEmblem, squadMaxSize, squadName} = request.body
-        const updatedValues = {squadAchievements, squadEmblem, squadMaxSize, squadName}
-        const previousSquad: Squad = await selectPartialSquadBySquadId(profileId) as Squad
-
-        const newSquad: Squad = {...previousSquad, ...updatedValues }
-        await updateSquad(newSquad)
-        return response.json({ status: 200, data: null, message: 'Squad successfully updated' })
-      } catch (error: any) {
-        return response.json({ status: 400, data: null, message: error.message })
+      if (profileId !== profileIdFromSession) {
+        return response.json({ status: 400, data: null, message: 'You are not allowed to preform this task' })
       }
+  // Export async function for updating a Squad
+      const {squadAchievements, squadEmblem, squadMaxSize, squadName} = request.body
+      const updatedValues = {squadAchievements, squadEmblem, squadMaxSize, squadName}
+      const previousSquad: Squad = await selectPartialSquadBySquadId(profileId) as Squad
+
+      const newSquad: Squad = {...previousSquad, ...updatedValues }
+      await updateSquad(newSquad)
+      return response.json({ status: 200, data: null, message: 'Squad successfully updated' })
+    } catch (error: any) {
+      return response.json({ status: 400, data: null, message: error.message })
+    }
 }
 
 // Export async function for getting a Squad by SquadId
--- backend/src/apis/squad/squad.route.ts
+++ backend/src/apis/squad/squad.route.ts
@@ -2,15 +2,17 @@ import { Router } from 'express';
 import { asyncValidatorController } from '../../utils/controllers/async-validator.controller';
 import {isLoggedIn} from "../../utils/controllers/isLoggedIn.controller";
 import { check, checkSchema } from 'express-validator'
-import { putSquadController, getSquadBySquadId, } from './squad.controller'
+import { putSquadController, getSquadBySquadId, postSquadController} from './squad.controller'
 import { squadValidator } from './squad.validator'
 
 export const squadRouter = Router();
 //Route for making a Squad.
-// squadRouter.route('/')
-//   .post(isLoggedIn, asyncValidatorController(checkSchema(squadValidator)), postSquadController)
+squadRouter.route('/')
+  .post(isLoggedIn, asyncValidatorController(checkSchema(squadValidator)), postSquadController)
 
 //Router for getting Squad by SquadId.
 squadRouter.route('/:squadId')
   .get(asyncValidatorController([check('squadId', 'please provide a valid squadId').isUUID()]), getSquadBySquadId)
+
+
   .put(isLoggedIn, asyncValidatorController(checkSchema(squadValidator)), putSquadController)
