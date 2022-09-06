50f17dbf9c8a08efcc53f50c4f43039cdfe58c7c
Tasha Deschenie <tdeschenie@cnm.edu>
2022-09-06 15:50:45 -0600

    added to data downloader file

---
+++ backend/src/utils/data-downloaders/data-downloader.ts
@@ -0,0 +1,26 @@
+import axios from 'axios'
+
+function dataDownloader():Promise <any>{
+  async function main(){
+    try {
+    await downloadGames()
+    } catch(error){
+      console.error(error)
+    }
+  }
+  return main()
+  async function downloadGames(){
+    const result = await axios({
+      url: "https://api.igdb.com/v4/games",
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Client-ID':process.env.TWITCH_ID,
+        'Authorization': `Bearer ${process.env.TWITCH_SECRET}`
+      },
+      data: "fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_modes,genres,hypes,involved_companies,keywords,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites;"
+    })
+    console.log(result)
+  }
+}
+dataDownloader().catch(error=>console.error(error))
