725ac48bfb4f2cfbdaea64af363031b877fc5260
CMcNeil <108107540+ClydeMcNeil25@users.noreply.github.com>
2022-09-07 15:12:16 -0500

    Added navigation to App.js

--- frontend/src/ui/App.js
+++ frontend/src/ui/App.js
@@ -8,10 +8,12 @@ import { SignUp } from './Sign-Up'
 import { Profile } from './Profile'
 import { Squads } from './Squads'
 import { SquadLead } from './SquadLead'
+import { Navigation } from './Navigation'
 
 export const App = () => (
   <>
     <BrowserRouter>
+      <Navigation/>
       <Routes>
         <Route  path='/' element={<Home />} />
         <Route path="*" element={<FourOhFour />} />
