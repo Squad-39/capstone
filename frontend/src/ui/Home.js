d91b8c15811b1d440b1bdb11417388ae5e5cb645
CMcNeil <108107540+ClydeMcNeil25@users.noreply.github.com>
2022-09-10 16:05:21 -0500

    added homepage layout with image and styling

--- frontend/src/ui/Home.js
+++ frontend/src/ui/Home.js
@@ -1,24 +1,14 @@
 import React from 'react'
 import Logo from '../ui/images/squadfinderlogo.png'
 import './styles/style.css'
-import Controller from './images/controllericon.png'
 
 export const Home = () => {
   return (
     <>
-      <section className="background">
-
-      <container>
-
-      <h1 className="text p-5">Welcome to Squad Finder</h1>
-
-      <img src={Logo} className='homeImage w-50' alt='squadLogo' />
-
-      </container>
-
-
-      </section>
-
+      <container className={""}>
+              <h1 className={"text text-center"}>Welcome to Squad Finder</h1>
+              <img src={Logo} className={"homeImage"} alt='squadLogo' />
+        </container>
     </>
   )
 }
--- frontend/src/ui/Navigation.js
+++ frontend/src/ui/Navigation.js
@@ -1,22 +1,27 @@
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import './styles/style.css'
+import { Container } from 'react-bootstrap'
 
 export function Navigation () {
   return (
     <>
-      <Navbar className='background' variant='dark'>
-        <Navbar.Brand href='/' className="text">Squad</Navbar.Brand>
-        <Nav className='me-auto'>
-          <Nav.Link href='/sign-in' className="text">Sign In</Nav.Link>
-          <Nav.Link href='/sign-up' className="text">Sign Up</Nav.Link>
-          <Nav.Link href='/squads' className="text">Squads</Nav.Link>
-          <Nav.Link href='/profile' className="text">Profile</Nav.Link>
-          <Nav.Link href='#aboutus' className="text">About Us</Nav.Link>
-          <Nav.Link href='#contactus' className="text">Contact Us</Nav.Link>
-        </Nav>
+      <Navbar className="background" expand="md">
+        <Container>
+          <Navbar.Brand href="/" className="text">Squad</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/sign-in" className="text">Sign In</Nav.Link>
+              <Nav.Link href="/sign-up" className="text">Sign Up</Nav.Link>
+              <Nav.Link href="/squads" className="text">Squads</Nav.Link>
+              <Nav.Link href="/profile" className="text">Profile</Nav.Link>
+              <Nav.Link href="/aboutus" className="text">About Us</Nav.Link>
+              <Nav.Link href="/contactus" className="text">Contact Us</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
-      <br />
     </>
   )
 }
