705b609978ecb6841fc59d15801a40ed6689b5a4
Jacob <janderson77@cnm.edu>
2022-09-11 10:17:28 -0600

    added games to database

---
+++ frontend/src/ui/games/statements.sql
@@ -0,0 +1,50 @@
+INSERT INTO game("gameId", "gameGenre", "gameImageUrl", "gameName")
+VALUES (gen_random_uuid(), 'MOBA', 'https://images.igdb.com/igdb/image/upload/t_cover_big/co49wj.png', 'League of Legends');
+
+INSERT INTO game("gameId", "gameGenre", "gameImageUrl", "gameName")
+VALUES (gen_random_uuid(), 'Action', 'https://images.igdb.com/igdb/image/upload/t_cover_big/co4cfe.png', 'Rocket League');
+
+INSERT INTO game("gameId", "gameGenre", "gameImageUrl", "gameName")
+VALUES (gen_random_uuid(), 'FPS', 'https://images.igdb.com/igdb/image/upload/t_cover_big/co4apq.png', 'Counter-Strike: Global Offensive');
+
+INSERT INTO game("gameId", "gameGenre", "gameImageUrl", "gameName")
+VALUES (gen_random_uuid(), 'MOBA', 'https://images.igdb.com/igdb/image/upload/t_cover_big/co4bko.png', 'Dota 2');
+
+INSERT INTO game("gameId", "gameGenre", "gameImageUrl", "gameName")
+VALUES (gen_random_uuid(), 'Fighting', 'https://images.igdb.com/igdb/image/upload/t_cover_big/co1w4f.png', 'Tekken 7');
+
+INSERT INTO game("gameId", "gameGenre", "gameImageUrl", "gameName")
+VALUES (gen_random_uuid(), 'Shooter', 'https://images.igdb.com/igdb/image/upload/t_cover_big/co1z26.png', 'Fortnite');
+
+INSERT INTO game("gameId", "gameGenre", "gameImageUrl", "gameName")
+VALUES (gen_random_uuid(), 'Shooter', 'https://images.igdb.com/igdb/image/upload/t_cover_big/co4c1x.png', 'Player Unknown Battlegrounds');
+
+INSERT INTO game("gameId", "gameGenre", "gameImageUrl", "gameName")
+VALUES (gen_random_uuid(), 'FPS', 'https://images.igdb.com/igdb/image/upload/t_cover_big/co1rcb.png', 'Overwatch');
+
+INSERT INTO game("gameId", "gameGenre", "gameImageUrl", "gameName")
+VALUES (gen_random_uuid(), 'FPS', 'https://images.igdb.com/igdb/image/upload/t_cover_big/co4x3b.png', 'Rainbow Six Siege');
+
+INSERT INTO game("gameId", "gameGenre", "gameImageUrl", "gameName")
+VALUES (gen_random_uuid(), 'Card', 'https://images.igdb.com/igdb/image/upload/t_cover_big/co1sh2.png', 'Hearthstone');
+
+INSERT INTO game("gameId", "gameGenre", "gameImageUrl", "gameName")
+VALUES (gen_random_uuid(), 'FPS', 'https://images.igdb.com/igdb/image/upload/t_cover_big/co1wzo.png', 'Apex Legends');
+
+INSERT INTO game("gameId", "gameGenre", "gameImageUrl", "gameName")
+VALUES (gen_random_uuid(), 'RTS', 'https://images.igdb.com/igdb/image/upload/t_cover_big/co49zh.png', 'Starcraft 2');
+
+INSERT INTO game("gameId", "gameGenre", "gameImageUrl", "gameName")
+VALUES (gen_random_uuid(), 'Fighting', 'https://images.igdb.com/igdb/image/upload/t_cover_big/co21yv.png', 'Super Smash Brothers');
+
+INSERT INTO game("gameId", "gameGenre", "gameImageUrl", "gameName")
+VALUES (gen_random_uuid(), 'Shooter', 'https://images.igdb.com/igdb/image/upload/t_cover_big/co2mvt.png', 'Valorant');
+
+INSERT INTO game("gameId", "gameGenre", "gameImageUrl", "gameName")
+VALUES (gen_random_uuid(), 'Sports Game', 'https://images.igdb.com/igdb/image/upload/t_cover_big/co4zw5.png', 'FIFA');
+
+INSERT INTO game("gameId", "gameGenre", "gameImageUrl", "gameName")
+VALUES (gen_random_uuid(), 'FPS', 'https://images.igdb.com/igdb/image/upload/t_cover_big/co20o8.png', 'Call of Duty Warzone');
+
+INSERT INTO game("gameId", "gameGenre", "gameImageUrl", "gameName")
+VALUES (gen_random_uuid(), 'Sports Game', 'https://images.igdb.com/igdb/image/upload/t_cover_big/co4th3.png', 'Madden');
