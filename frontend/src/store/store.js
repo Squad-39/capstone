98d310dcfe8b56ee4a3550544dd9fc4002ca046a
Jacob <janderson77@cnm.edu>
2022-09-13 08:56:24 -0600

    fixed merge conflicts

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,10 +1,8 @@
 import { configureStore,combineReducers} from '@reduxjs/toolkit'
 import auth from './auth'
 import squads from './squads.js'
-<<<<<<< HEAD
-const reducer = combineReducers({auth, squads})
-=======
 import games from "./games";
-const reducer = combineReducers({auth, squads, games})
->>>>>>> style-squad
+import profile from "./profile"
+import squaddetails from './squaddetails'
+const reducer = combineReducers({auth, squads, squaddetails, games, profile})
 export default configureStore({reducer});
--- frontend/src/ui/App.js
+++ frontend/src/ui/App.js
@@ -3,66 +3,41 @@ import 'bootstrap/dist/css/bootstrap.css'
 import { Home } from './Home'
 import React from 'react'
 import { SignUp } from './Sign-Up'
-<<<<<<< HEAD
-import { Profile } from './Profile'
-import { Squads } from './Squads'
-import { SquadLead } from './SquadLead'
-import { Navigation } from './Navigation'
-import { SignIn } from './sign-in/Sign-In'
-=======
 import { Profile } from './profile/Profile'
 import { Squads } from './squads/Squads'
 import { SquadLead } from './SquadLead'
 import { Navigation } from './Navigation'
-import { SignIn } from './Sign-In'
->>>>>>> style-squad
+import { SignIn } from './sign-in/Sign-In'
 import { Provider } from 'react-redux'
 import './styles/style.css'
+import { SquadDetailPage } from './squadDetail/SquadDetailPage'
 
 
 
 export const App = ({store}) => (
-<<<<<<< HEAD
-=======
 
->>>>>>> style-squad
+
+
   <>
     <Provider store={store}>
-    <BrowserRouter>
-      <Navigation />
-<<<<<<< HEAD
-      <section className="background">
-      <Routes>
+      <BrowserRouter>
+        <Navigation />
 
-        <Route exact path="/squad/:squadId" element={<SquadPosts />} squadId=":squadId"/>
+        <section className="background">
+          <Routes>
 
-        <Route path='/' element={<Home />} />
-        <Route path='/sign-in' element={<SignIn />} />
-        <Route path='/sign-up' element={<SignUp />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/squads' element={<Squads />} />
-        <Route path='/squadlead' element={<SquadLead />} />
-
-=======
-
-      <section className="background">
-
-      <Routes>
+            <Route exact path="/squad-details/:squadId" element={<SquadDetailPage />} squadId=":squadId"/>
+            <Route path='/' element={<Home />} />
+            <Route path='/sign-in' element={<SignIn />} />
+            <Route path='/sign-up' element={<SignUp />} />
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/squads' element={<Squads />} />
+            <Route path='/squadlead' element={<SquadLead />} />
 
 
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/squads" element={< Squads />} />
-        <Route path="/squadlead" element={< SquadLead />} />
-
-        <Route path='/' element={<Home />} />
-        <Route path='*' element={<FourOhFour />} />
-        <Route path='/sign-in' element={<SignIn />} />
-        <Route path='/sign-up' element={<SignUp />} />
-
->>>>>>> style-squad
-      </Routes>
-      </section>
-    </BrowserRouter>
+          </Routes>
+        </section>
+      </BrowserRouter>
     </Provider>
   </>
 )
